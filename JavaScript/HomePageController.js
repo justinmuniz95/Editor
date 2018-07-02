12640b39e9cc587bbc8e122935a07f1fc09a1061
root <root@Just.localdomain>
2018-07-02 00:15:41 -0400

    add font slider and border

--- JavaScript/HomePageController.js
+++ JavaScript/HomePageController.js
@@ -3,16 +3,6 @@ app.controller('homeController', function($scope) {
     //$scope.colors = ["purple", "blue", "green", "pink", "white", "black"];
 });
 
-/*function downloadInnerHtml(filename, elId){
-  var ehtml = document.getElementById(elId).innerHTML;
-  var link = document.createElement('a');
-  link.setAttribute('download', filename);
-  link.setAttribute('href', 'data:', + 'text/doc' + ';charset=utf-8,' + encondeURIComponet(elHTML));
-//  link.click();
-  //var filename= 'tags.doc';
-//  downloadInnerHtml(filename, 'main');
-}*/
-
 function centerP() {
     document.getElementById("myP").style.textAlign = "center";
 }
